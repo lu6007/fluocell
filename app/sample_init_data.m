0f1b1ae0836cb64fb49da3ba08edf1853c11981a
Kathy Lu <shaoying.lu@gmail.com>
2016-07-15 01:45:37 -0700

    Minor corrections

--- app/sample_init_data.m
+++ app/sample_init_data.m
@@ -1,6 +1,7 @@
 % initialize data  for the various applications.
 function data = sample_init_data(name, function_name)
-root = '..\..\data\fluocell\';
+% replace the root variable in line 4 with the location of fluocell_data\ folder
+root = '..\..\data\fluocell_sample\';
 switch name,
     case 'src_pax',
         % General parameters
