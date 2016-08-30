84e23ed33d8c6e8a48aa0fe50b9ad51968f63543
sjpl <slaub@ucsd.edu>
2016-08-30 10:29:48 -0700

    Added the z-index to figure titles when 3D viewing is enabled.

--- src/api/my_title.m
+++ src/api/my_title.m
@@ -2,6 +2,17 @@
 % into all the titles
 
 % Copyright: Shaoying Lu and Yingxiao Wang 2014
-function my_title(title_name, index)
-title(strcat(title_name, '\_', num2str(index))); 
+function my_title(title_name, index, varargin)
+%Enabled possible display of z-index for the title. - Shannon 8/24/2016
+parameter_name = {'data'};
+default_value = {[]};
+[data] = parse_parameter(parameter_name, default_value, varargin);
+
+%Check if the z-stack viewer is in use.
+if ~(isfield(data,'image_type') && strcmp(data.image_type,'z-stack'))
+    title(strcat(title_name, '\_', num2str(index))); %Default behavior.
+else
+    title(strcat(title_name,'\_', num2str(index),'\_z',num2str(data.z_index))); %Viewing z-stack.
+end
+
 return;
