1e46afc650d0b76092ba8e581c27fb000653ca03
YuxinShi0423 <yus155@eng.ucsd.edu>
2017-05-15 10:22:53 -0700

    Update init_figure.m

--- src/api/init_figure.m
+++ src/api/init_figure.m
@@ -4,43 +4,43 @@
 
 % Copyright: Shaoying Lu and Yingxiao Wang 2012
 function data = init_figure(data)
-switch data.protocol,
-    case 'FRET',
+switch data.protocol
+    case 'FRET'
         num_figures = 2;
 %         if ~isfield(data,'is_channel1_over_channel2'),
 %             data.is_channel1_over_channel2 = 1;
 %         end;
-    case 'FRET-Intensity',
+    case 'FRET-Intensity'
         num_figures = 3;
 %         if ~isfield(data,'is_channel1_over_channel2'),
 %             data.is_channel1_over_channel2 = 1;
 %         end;
-    case 'FRET-Intensity-2',
+    case 'FRET-Intensity-2'
         num_figures = 4;
 %         if ~isfield(data,'is_channel1_over_channel2'),
 %             data.is_channel1_over_channel2 = 1;
 %         end;
-    case 'FRET-DIC',
+    case 'FRET-DIC'
         num_figures = 3;
 %         if ~isfield(data,'is_channel1_over_channel2'),
 %             data.is_channel1_over_channel2 = 1;
 %         end;
-    case 'FRET-Intensity-DIC',
+    case 'FRET-Intensity-DIC'
         num_figures = 4;
 %         if ~isfield(data,'is_channel1_over_channel2'),
 %             data.is_channel1_over_channel2 = 1;
 %         end;
-    case {'FLIM', 'STED'}, 
+    case {'FLIM', 'STED'}
         num_figures = 2;
-    case 'Intensity',
+    case 'Intensity'
         num_figures = 2;
-    case 'Intensity-DIC',
+    case 'Intensity-DIC'
         num_figures = 3;
 end;
 data.num_figures = num_figures;
-if ~isfield(data, 'f'),
+if ~isfield(data, 'f')
     f = zeros(num_figures, 1);
-    for i =1:num_figures,
+    for i =1:num_figures
         f(i) = figure;
         set(gcf, 'ColorMap', jet);
         set(gca, 'FontSize', 12, 'FontWeight','bold',...
