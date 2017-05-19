9cf470342b1ac279807592f66cde290313d2b790
lu6007 <shaoying.lu@gmail.com>
2017-05-19 11:54:12 -0700

    Remove duplicates
    Added the pdf User’s Guide and removed duplicated display_kstest.m

--- src/utility/display_kstest.m
+++
@@ -1,8 +0,0 @@
-% function display_kstest(v1, v2, str)
-
-% Copyright: Shaoying Lu and Yingxiao Wang 2011
-function display_kstest(v1, v2, str)
-n1 = length(v1); n2 = length(v2);
-[h p] = kstest2(v1,v2);
-display(strcat(str, num2str(p), ', ', num2str(n1), ', ', num2str(n2)));
-return;
