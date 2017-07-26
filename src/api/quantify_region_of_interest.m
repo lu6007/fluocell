8fe94370aae10ab52f547c939f6571af114f92c2
sjpl <slaub@ucsd.edu>
2017-07-26 16:18:03 -0700

    Code cleanup for quantify_region_of_interest.m

--- src/api/quantify_region_of_interest.m
+++ src/api/quantify_region_of_interest.m
@@ -21,7 +21,8 @@ default_value = {0};
 [save_bw_file] = parse_parameter(parameter_name, default_value, varargin);
 %Lexie on 3/10/2015 make the show_figure option work for both situations,
 %w/o show_figure field
-show_figure_option = ~isfield(data, 'show_figure') || data.show_figure;
+% show_figure_option = ~isfield(data, 'show_figure') || data.show_figure;
+
 % Process data.quantify_roi
 switch data.quantify_roi
     case 0 % Do not quantify ROI
@@ -40,11 +41,6 @@ end
 % data.quantify_roi = 2: more than 1 roi which can only be automatically tracked.  
 if data.quantify_roi == 1 || data.quantify_roi ==2
     num_object = 1;
-    if isfield(data,'num_roi')
-        num_roi = data.num_roi;
-    else
-        num_roi = 1;
-    end
 end
 
 if isfield(data, 'num_roi')
