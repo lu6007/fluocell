cebe283cc3cca88a56ad188e9fe718d32a232e43
lu6007 <shaoying.lu@gmail.com>
2017-08-09 23:16:42 -0700

    Allow automatic detection of intensity_bound.

--- src/vis/get_imd_image.m
+++ src/vis/get_imd_image.m
@@ -1,7 +1,7 @@
 % function get_imd_image(im_color, im_intensity, varargin)
 % parameter_names = {'ratio_bound', 'intensity_bound'};
 % default_values ={[min(min(im_color)), max(max(im_color))],...
-%     [min(min(im_intensity)), max(max(im_intensity))]};
+%     default_intensity_bound};
 %
 % Converts gray scale images im_color and im_intensity into a color image
 % with the IMD display. 
@@ -9,11 +9,22 @@
 % Copyright: Shaoying Lu and Yingxiao Wang 2011
 
 function imd_image = get_imd_image(im_color, im_intensity, varargin)
+%
+alpha = 0.5;
+min_i = min(min(im_intensity));
+max_i = max(max(im_intensity));
+default_intensity_bound = [min_i, min_i+alpha*(max_i-min_i)];
+%
 parameter_name = {'ratio_bound', 'intensity_bound'};
 default_value ={[min(min(im_color)), max(max(im_color))],...
-    [min(min(im_intensity)), max(max(im_intensity))]};
+    default_intensity_bound};
 [ratio_bound, intensity_bound] = ...
     parse_parameter(parameter_name, default_value, varargin);
+
+if isempty(intensity_bound)
+    intensity_bound = default_intensity_bound;
+end
+
 load 'my_hsv.mat';
 cbound = [1 length(my_hsv)];
 temp = floor(imscale(im_color, cbound(1), cbound(2), ratio_bound));
