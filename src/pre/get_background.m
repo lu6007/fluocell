a0e687d23ee25cb402a5f847343c8b8722782302
lu6007 <shaoying.lu@gmail.com>
2017-10-04 10:58:57 -0700

    Bug fix

--- src/pre/get_background.m
+++ src/pre/get_background.m
@@ -41,59 +41,64 @@ value = sum(sum(double(im)/sum_bw.*double_bw));
 return
 
 function [bg_bw, bg_poly] = get_background_auto(im, file_bg)
-% title_str = 'Automatically detect the background region.';
-num_polygon = 4;
-[num_row, num_col] = size(im);
-bw = cell(num_polygon, 1);
-poly = cell(num_polygon, 1); 
+if exist(file_bg, 'file')
+    res = load(file_bg);
+    bg_bw = res.bw;
+    bg_poly = res.poly;
+else
+    num_polygon = 4;
+    [num_row, num_col] = size(im);
+    bw = cell(num_polygon, 1);
+    poly = cell(num_polygon, 1); 
 
-ss = 20; % size-1
-%first region
-x = [1; 1+ss; 1+ss; 1; 1];
-y = [1; 1; 1+ss; 1+ss; 1];
-bw{1} = roipoly(im,x,y); 
-poly{1} = [x y]; clear x y;
+    ss = 20; % size-1
+    %first region: top left corner
+    x = [1; 1+ss; 1+ss; 1; 1];
+    y = [1; 1; 1+ss; 1+ss; 1];
+    bw{1} = roipoly(im,x,y); 
+    poly{1} = [x y]; clear x y;
 
-%second region
-x = [num_col-ss; num_col; num_col; num_col-ss; num_col-ss];
-y = [1; 1; 1+ss; 1+ss; 1];
-bw{2} = roipoly(im,x,y); 
-poly{2} = [x y]; clear x y;
+    %second region: top right corner
+    x = [num_col-ss; num_col; num_col; num_col-ss; num_col-ss];
+    y = [1; 1; 1+ss; 1+ss; 1];
+    bw{2} = roipoly(im,x,y); 
+    poly{2} = [x y]; clear x y;
 
-%third region
-x = [1; 1+ss; 1+ss; 1; 1];
-y = [num_row-ss; num_row-ss; num_row; num_row; num_row-ss];
-bw{3} = roipoly(im,x,y); 
-poly{3} = [x y]; clear x y;
+    %third region: lower left corner
+    x = [1; 1+ss; 1+ss; 1; 1];
+    y = [num_row-ss; num_row-ss; num_row; num_row; num_row-ss];
+    bw{3} = roipoly(im,x,y); 
+    poly{3} = [x y]; clear x y;
 
-%fourth region
-x = [num_col-ss; num_col; num_col; num_col-ss; num_col-ss];
-y = [num_row-ss; num_row-ss; num_row; num_row; num_row-ss];
-bw{4} = roipoly(im,x,y); 
-poly{4} = [x y]; clear x y;
+    %fourth region: lower right corner
+    x = [num_col-ss; num_col; num_col; num_col-ss; num_col-ss];
+    y = [num_row-ss; num_row-ss; num_row; num_row; num_row-ss];
+    bw{4} = roipoly(im,x,y); 
+    poly{4} = [x y]; clear x y;
 
-%             for j = 1:4,
-%                 plot(poly{j}(:,1),poly{j}(:,2),'k','LineWidth',1);
-%             end
+    %             for j = 1:4,
+    %                 plot(poly{j}(:,1),poly{j}(:,2),'k','LineWidth',1);
+    %             end
 
-% calculate the intensity at 4 corners and pick the region with
-% the minimal value
-min_index = 0;
-min_intensity = 10^10;
-for j = 1:4
-   intensity = sum(sum(double(im).*double(bw{j})))/sum(sum(double(bw{j})));
-   if intensity< min_intensity
-       min_index = j;
-       min_intensity = intensity;
-   end
-end
+    % calculate the intensity at 4 corners and pick the region with
+    % the minimal value
+    min_index = 0;
+    min_intensity = 10^10;
+    for j = 1:4
+       intensity = sum(sum(double(im).*double(bw{j})))/sum(sum(double(bw{j})));
+       if intensity< min_intensity
+           min_index = j;
+           min_intensity = intensity;
+       end
+    end
 
-bg_bw{1} = bw{min_index};
-bg_poly{1} = poly{min_index};
-%            h = figure; imagesc(im); title(title_str); hold on;
-%            plot(poly{1}(:,1), poly{1}(:,2), 'r', 'LineWidth', 1);
-%            close(h);
-if ~isempty(file_bg)
+    bg_bw{1} = bw{min_index};
+    bg_poly{1} = poly{min_index};
+    %            h = figure; imagesc(im); title(title_str); hold on;
+    %            plot(poly{1}(:,1), poly{1}(:,2), 'r', 'LineWidth', 1);
+    %            close(h);
+    clear bw; clear poly;
+    bw = bg_bw; poly = bg_poly;
     save(file_bg, 'bw', 'poly');  
 end
 
